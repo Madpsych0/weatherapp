da7f1bdf741b14a47e034b97cec0799ee12b906e
Madpsych0 <lovegamer151@gmail.com>
2025-06-10 19:22:47 +0530

    .

--- django-weather/weather_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.forecast, name='forecast'),
-    path('history/', views.history, name='history'),
-    path('about/', views.about, name='about'),
-]
